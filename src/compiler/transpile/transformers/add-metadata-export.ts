0d34a85fc679fd0627f57ab021064c802001a0f9
Adam Bradley <adambradley25@gmail.com>
2017-12-19 15:58:37 -0600

    wip

--- src/compiler/transpile/transformers/add-metadata-export.ts
+++ src/compiler/transpile/transformers/add-metadata-export.ts
@@ -3,6 +3,16 @@ import { ComponentMeta, ModuleFiles } from '../../../util/interfaces';
 import * as ts from 'typescript';
 
 
+/**
+ * 1) Add static "properties" from previously gathered component metadata
+ * 2) Add static "encapsulation"
+ * 3) Add static "host"
+ * 4) Add static "events"
+ * 5) Add h() fn: const { h } = Namespace;
+ * 6) Export component class with tag names as PascalCase
+ */
+
+
 export default function addMetadataExport(moduleFiles: ModuleFiles): ts.TransformerFactory<ts.SourceFile> {
 
   return (transformContext) => {
